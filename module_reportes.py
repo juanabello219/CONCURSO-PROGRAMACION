0d8391c1e8c209a286df1fdf8d01bb68d112c1e8
Luis Manuel Palmera Quintero <dfguerrero@unicesar.edu.co>
2023-05-19 20:17:11 -0500

    Update module_reportes.py
    update

--- module_reportes.py
+++ module_reportes.py
@@ -1,1 +1,18 @@
 #Funciones para manejar los reportes
+
+#Consultas por usuario
+def search_user(data):
+    pass
+
+def search_card(data):
+    pass
+
+def search_cards_actives(data):
+    pass
+
+def search_cards_inactives(data):
+    pass
+
+def search_reload_date(data):
+    pass
+
