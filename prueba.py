2ca2f2b02d37a637f266d2992f95800d56c103e8
juanabello219 <128939966+juanabello219@users.noreply.github.com>
2023-05-20 14:15:54 -0500

    commit
    archivo para correcciones y pruebas

--- prueba.py
+++ prueba.py
@@ -1,16 +1,28 @@
-cedula = "1098621411"
-nombre = "juan andres"
-apellido = "abello contreras"
-direccion = "gamarra cesar"
-telefono = "3157700754"
-
-usuario= {}
-cod_tarjeta=123789
-saldo =0
-estado=True
-
-usuario[cedula]={"nombre":nombre,"apellido":apellido,"direccion":direccion,"telefono":telefono,"tarjetas":[]}
-valor=usuario[cedula]
-valor["tarjetas"]=[cod_tarjeta,saldo,estado]
-
-print(usuario)
+def search_user(data_users):
+    cedula = input("Ingrese la cedula a consultar: ")
+    for row in data_users:
+        valor = list(row.keys())
+        #print(valor)
+        if valor[0] == cedula:
+            system_clear_function()
+            print("="*90)
+            print("                       DATOS DEL USUARIO                       ")
+            gap = ' '*3
+            encabezado = f"{'Cedula':^10s}{gap}{'Nombres y apellidos':^35s}{gap}{'Dirección':^15}{gap}{'Telefono':^10s}"
+            print("="*90)
+            print(encabezado)
+            print("-"*90)
+            datos_usuario=row[cedula]
+            #print(datos_usuario)
+            rec = f"{cedula:>10s}{gap}{datos_usuario['nombre']:17s}{datos_usuario['apellido']:18s}{gap}{datos_usuario['direccion']:^15}{gap}{datos_usuario['telefono']:^10}"
+            print(rec)
+            # print(f"TARJETAS A CARGO")
+            # cards = datos_usuario["tarjetas_a_cargo"]
+            # print(Template("$codigo"     "$saldo"     "$estado").substitute(codigo = "Codigo", saldo="Saldo", estado="Estado"))
+            # for i in cards:
+            #     print(Template("$codigo"     "$saldo"     "$estado").substitute(codigo = i[0], saldo=i[1], estado=i[2]))      
+            # print("")
+            return True
+        else:
+            print("El usuario no esta registrado en el sistema.")
+            return False
