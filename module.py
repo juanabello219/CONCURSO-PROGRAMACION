0af8ceb9704bb98a94bc65cb88a34c8c0760d0e3
Luis Manuel Palmera Quintero <dfguerrero@unicesar.edu.co>
2023-05-20 16:51:07 -0500

    cambios
    cambios

--- module.py
+++ module.py
@@ -11,12 +11,12 @@ datos_usuarios = [{
         "apellido":"Guerrero Sumalave", 
         "direccion":"Carrera 37 número 2 Norte 29", 
         "telefono":"3208777146", 
-        "tarjetas_a_cargo":[
+        "tarjetas":[
             [
-                890123,0,True
+                890123,0,"Activa"
             ],
             [
-                123890,10000,False
+                123890,10000,"Activa"
             ]
         ]
     } 
@@ -44,6 +44,79 @@ recargas = [
 
 codigos_tarjetas = []
 
+def recargas_tarjetas(datos_usuarios):
+    cedula = input("Ingrese el numero de su cedula: ")
+    for row in datos_usuarios:
+        #print(row)
+        valor = list(row.keys())
+        print(valor)
+        if valor[0] == cedula:
+            datos_usuario=row[cedula]
+            print(f"{datos_usuario['nombre']} {datos_usuario['apellido']}")
+            tarjetas=datos_usuario["tarjetas"]
+            print("="*90)
+            print("                       DATOS DE LAS TARJETAS                       ")
+            gap = ' '*3
+            encabezado = f"{'Codigo':^6s}{gap}{'Saldo':^10s}{gap}{'Estado':^10}"
+            print("="*32)
+            print(encabezado)
+            print("-"*32)
+            for i in tarjetas:
+                rec = f"{i[0]:<6d}{gap}{i[1]:10d}{gap}{i[2]:>10s}"
+                print(rec)
+            cod_recarga=int(input("Ingrese el codigo de la tarjeta a recargar: "))
+            for i in tarjetas:
+                if i[0]==cod_recarga and i[2]=="Inactiva":
+                    print("Esta tarjeta esta inactiva, no se puede recargar")
+                    return
+                elif i[0]==cod_recarga and i[2]=="Activa":
+                    pos=tarjetas.index(i)
+                    cop_recarga=int(input("Ingrese el valor a recargar: "))
+                    tarjetas.pop(pos)
+                    print(tarjetas)
+                    temp=[i[0],i[1]+cop_recarga,"Activa"]
+                    tarjetas.append(temp)
+                    datos_usuario["tarjetas"]=tarjetas
+                    print(datos_usuario)
+                    print(datos_usuarios)
+                    return 
+
+def inactivar_tarjetas(datos_usuarios):
+    cedula = input("Ingrese el numero de su cedula: ")
+    for row in datos_usuarios:
+        #print(row)
+        valor = list(row.keys())
+        print(valor)
+        if valor[0] == cedula:
+            datos_usuario=row[cedula]
+            print(f"{datos_usuario['nombre']} {datos_usuario['apellido']}")
+            tarjetas=datos_usuario["tarjetas"]
+            print("="*90)
+            print("                       DATOS DE LAS TARJETAS                       ")
+            gap = ' '*3
+            encabezado = f"{'Codigo':^6s}{gap}{'Saldo':^10s}{gap}{'Estado':^10}"
+            print("="*32)
+            print(encabezado)
+            print("-"*32)
+            for i in tarjetas:
+                rec = f"{i[0]:<6d}{gap}{i[1]:10d}{gap}{i[2]:>10s}"
+                print(rec)
+            cod_tarjeta=int(input("Ingrese el codigo de la tarjeta a desactivar: "))
+            for i in tarjetas:
+                if cod_tarjeta ==i[0]:
+                    pos=tarjetas.index(i)
+                    confirmacion=input("Confirma inactivación de tarjeta: ").lower()
+                    if confirmacion=="si":
+                        tarjetas.pop(pos)
+                        print(tarjetas)
+                        temp=[i[0],i[1],"Inactiva"]
+                        tarjetas.append(temp)
+                        datos_usuario["tarjetas"]=tarjetas
+                        print(datos_usuario)
+                        print(datos_usuarios)
+                        return 
+        else:
+            print("Este usuario no esta registrado en el sistema.")
 
 
 #Juan toca mejorar esto pensando en la estructura que deje como base arriba.
@@ -80,17 +153,17 @@ def menu():
         print("2.    Comprar tarjeta                        ")
         print("3.    Registro de usuarios                   ")
         print("4.    Reportes generales                     ")
-        print("5.   inactivación de tarjeta                 ")
+        print("5.    Inactivación de tarjeta                ")
         print("6.    Salir del sistema                      ")
         print("                                             ")
         print("============================================ ")
         opcion = int(input("Ingrese la opción que deseas utilizar: "))
-        if (opcion>4):
+        if (opcion>6):
             system_clear_function()
             print("La opción no se encuentra, intentalo de nuevo.")
             continue
         if (opcion==1):
-            pass
+            recargas_tarjetas(datos_usuarios)
         elif (opcion==2):
             valor=compra_tarjetas()
             print(valor)
@@ -99,6 +172,8 @@ def menu():
             pass
         elif(opcion==4):
             reportes.menu_reportes(datos_usuarios)
+        elif (opcion==5):
+            inactivar_tarjetas(datos_usuarios)
         else:
             print("Ocurrio un error, Intentalo de nuevo.")
             continue
